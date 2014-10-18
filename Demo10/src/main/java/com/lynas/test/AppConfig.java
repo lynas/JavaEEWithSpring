f00f615f84d117c243aeba98b77abb373b989a90
lynas <szlynas@gmai.com>
2014-10-18 18:50:42 +0600

    first commit. contains basic java ee app with spring mvc framework

---
+++ Demo10/src/main/java/com/lynas/test/AppConfig.java
@@ -0,0 +1,20 @@
+package com.lynas.test;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+@Configuration
+public class AppConfig {
+	
+	@Bean
+	public InternalResourceViewResolver viewResolver(){
+		
+		InternalResourceViewResolver iRVR = new InternalResourceViewResolver();
+		iRVR.setPrefix("/WEB-INF/jsp/");
+		iRVR.setSuffix(".jsp");
+		return iRVR;
+		
+	}
+
+}
